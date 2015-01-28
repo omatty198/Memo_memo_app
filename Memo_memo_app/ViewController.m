b3a5445dc5c1a96ebe2ad0d8a7251afa01c17e9e
kolonzu <koushin07@live.jp>
2015-01-28 20:58:28 +0900

    こ

--- Memo_memo_app/ViewController.m
+++ Memo_memo_app/ViewController.m
@@ -10,9 +10,11 @@
 
 @interface ViewController ()
 {
-    int count;
-    int x;
-    int y;
+    int count;//test
+    int x_origin;//cellの位置xを決定するための3メモ間共通の変数
+    int y_origin;//
+    int x_2nd;//cellの位置y(決定用
+    int y_2nd;//
 }
 @end
 
@@ -28,31 +30,29 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)timer_custom_viewview{
-    count=arc4random_uniform(9);
+- (IBAction)timer_custom_viewview{//タイマーcell
+//    count=arc4random_uniform(9);
     UIView *subview = [[NSBundle mainBundle] loadNibNamed:@"timer_custom_view" owner:self options:nil][0];
-    subview.frame = CGRectMake(100, count * 100, 100, 100);
-
+    subview.frame = CGRectMake(x_2nd, y_2nd , 100, 100);
+    [self seiretu];
     [self setRandomColor:subview];
     [self.view addSubview:subview];
 }
-- (IBAction)add_memo_custom_view{
-    count=arc4random_uniform(9);
+- (IBAction)add_memo_custom_view{//メモcell
     UIView *subview = [[NSBundle mainBundle] loadNibNamed:@"memo_custom_view" owner:self options:nil][0];
-    subview.frame = CGRectMake(count *100, 100, 100, 100);
-
+    subview.frame = CGRectMake(x_2nd, y_2nd, 100, 100);
+    [self seiretu];
     [self setRandomColor:subview];
     [self.view addSubview:subview];
 }
-- (IBAction)photo_custom_view{
-    count=arc4random_uniform(9);
+- (IBAction)photo_custom_view{//フォトcell
     UIView *subview = [[NSBundle mainBundle] loadNibNamed:@"photo_custom_view" owner:self options:nil][0];
-    subview.frame = CGRectMake(100, count *100, 100, 100);
-    
+    subview.frame = CGRectMake(x_2nd, y_2nd, 100, 100);
+    [self seiretu];
     [self setRandomColor:subview];
     [self.view addSubview:subview];
 }
-- (void)setRandomColor:(UIView *)subview {
+- (void)setRandomColor:(UIView *)subview {//色
     CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
     CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
     CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
@@ -60,12 +60,25 @@
     subview.backgroundColor = color;
 }
 
--(void)seiretu{
-    x++;
-    if(x==3){
-        x=1;
+-(void)seiretu{//cellを並ばせます。ある意味一番大切？な所です
+    x_origin++;
+    if(x_origin%3==0){//横列に全部入った場合
+        y_origin++;//y座標を下げる
+        if(x_origin==3 && y_origin==5)//ジェスチャゾーンに入りそうな場合（将来的には画面スクロールできるようにするから消す
+        {
+            UIAlertView *alert =
+            [[UIAlertView alloc] initWithTitle:@"お知らせ" message:@"増やしすぎ!!"
+                                      delegate:self cancelButtonTitle:@"わかった。" otherButtonTitles:nil];
+            [alert show];
+        }
+        x_origin=0;//メモが入りきれないのでx座標を戻す
+        x_2nd=137.5*x_origin;
+        y_2nd= 120*y_origin;
+    }
+    else{//通常時
+        x_2nd=137.5*x_origin;
+        
     }
     
-
 }
 @end
