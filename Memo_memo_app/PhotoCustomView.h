d27d05e4b74c7aa6f009d873f960f3aa82dad7d1
ikuya omatsu <133w304w@stu.kobe-u.ac.jp>
2015-02-21 13:05:13 +0900

    UIImagePickerControllerを使えるようにする準備

---
+++ Memo_memo_app/PhotoCustomView.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCustomView.h
+//  Memo_memo_app
+//
+//  Created by ikuya omatsu on 2015/02/21.
+//  Copyright (c) 2015年 kolonzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCustomView : UIView
+@property (nonatomic,weak) IBOutlet UIImageView *photoImageView;
+@end
---
+++ Memo_memo_app/PhotoCustomView.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCustomView.m
+//  Memo_memo_app
+//
+//  Created by ikuya omatsu on 2015/02/21.
+//  Copyright (c) 2015年 kolonzu. All rights reserved.
+//
+
+#import "PhotoCustomView.h"
+
+@implementation PhotoCustomView
+
+@end
